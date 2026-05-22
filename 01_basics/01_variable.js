1aa2214ae6ca4107ce4affbd06c958b2358683a6
kundangond1503-debug <kundangond1503@gmail.com>
2026-05-22 10:59:44 +0000

    data types

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,14 @@
+const accountId = 144553
+let accountEmail = "kundangond@gamail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+
+// accountId = 2
+accountEmail = "hsc@gamil.com"
+accountPassword = "5454545"
+accountCity = "bangluru"
+
+console.log(accountId)
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
