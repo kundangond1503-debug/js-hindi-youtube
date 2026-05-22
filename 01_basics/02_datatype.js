6397dc0d3cdea349ab00beecddfa177188bd09d4
kundangond1503-debug <kundangond1503@gmail.com>
2026-05-22 11:00:47 +0000

    data types

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,18 @@
+"use strict"; // treat all js as newer version
+
+console.log("kundan")
+
+let name = "kundan"
+let age = 18
+let isLoggedIn = false 
+
+//number => 2  to power 53
+//bigint use hota hai jb number bada ho
+//string => ""
+//boolean => true / false 
+//null => standalone value
+//undefined =>
+//symbol => unique
+// typeof btata hai ki kaunsa data type hai
+// object
+
