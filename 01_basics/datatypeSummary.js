3f8e3f002f7b2da1621ad66b1be5ae9e987994c1
kundangond1503-debug <kundangond1503@gmail.com>
2026-05-23 10:22:41 +0000

    datatypeSummary

---
+++ 01_basics/datatypeSummary.js
@@ -0,0 +1,33 @@
+// primitive
+
+// 7 types : string, Number, Boolean, Null, symbol=>kisi chij ko unique bnane ke liye use hota hai, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null;
+let useremail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id == anotherId)
+const bigNumber = 3463724729688542729n
+
+// reference (Non primitive)
+
+// array, objects, function
+
+const heros = ["shaktiman", "naagraj", "doga"];
+let myObj = {
+    name : Kundan,
+    age : 21,
+}
+
+const myFunction = function(){
+    console.log("Hello world"); 
+}
+
+
+
