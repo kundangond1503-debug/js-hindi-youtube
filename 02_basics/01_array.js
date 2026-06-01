4fbd48a44039d15c099e5afbd5bd96f7ec012702
kundangond1503-debug <kundangond1503@gmail.com>
2026-06-01 10:46:37 +0000

    array

---
+++ 02_basics/01_array.js
@@ -0,0 +1,35 @@
+const myArr = [0, 1, 2, 3, 4, 5]
+const myHeros = ["Shaktiman", "nagraj"]
+
+const myArr2 = new Array(1, 2, 3, 4)
+console.log(myArr[2])
+
+//  ARRAY METHOD
+
+// myArr.push(6);
+// console.log(myArr);
+// myArr.pop();
+
+// myArr.unshift(9);
+// myArr.shift();
+// console.log(myArr);
+
+console.log(myArr.includes(9));
+console.log(myArr.indexOf(8));
+
+const newArr = myArr.join()
+
+console.log(myArr);
+console.log(newArr);
+
+// SLICE , SPLICE
+
+console.log("A",myArr);
+const myn1 = myArr.slice(1, 3);
+
+console.log(myn1);
+console.log("B",myArr);
+
+const myn2 = myArr.splice(1,3)
+console.log("C",myArr);
+console.log(myn2);
