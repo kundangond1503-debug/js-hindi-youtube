a0c987a09e82cf9bdfa5092bb344eff3994bddeb
kundangond1503-debug <kundangond1503@gmail.com>
2026-06-05 10:20:56 +0000

    control flow

---
+++ 04_control_flow/one.js
@@ -0,0 +1,53 @@
+// const isUserloggedIn = true
+// const temperature = 41
+
+// if(temperature ===41){
+//     console.log("less than 50");
+    
+// }else {
+//     console.log("temperature is greater than 50");
+    
+// }
+
+// console.log("executed");
+
+// const score = 200;
+
+// if(score>100){
+//     const power = "fly";
+//     console.log(`user power: ${power}`);
+    
+// }
+
+// console.log(`user power : ${power}`);
+
+const balance = 1000;
+// if(balance > 500)console.log("test"),console.log("test2");
+
+// if(balance < 500){
+//     console.log("less than 500");
+    
+// }else if(balance<750){
+//     console.log("less than 750");
+    
+// }else if(balance<900){
+//     console.log("less than 900");
+// }else{
+//     console.log("less than 1200");
+    
+// }
+
+
+const isUserloggedIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromemail = true;
+
+if(isUserloggedIn && debitCard && 2==2){
+    console.log("allow to buy course");
+    
+}
+
+if(loggedInFromGoogle || loggedInFromemail){
+    console.log("User logged in")
+}
